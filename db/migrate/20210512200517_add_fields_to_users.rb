5080d4f75f10a1bef9d41af3a9ef382ca3171d0e
Christian <chrisdrobbo@gmail.com>
2021-05-18 22:32:13 +0100

    rolled back database migrations and fixed typo

--- db/migrate/20210512200517_add_fields_to_users.rb
+++ db/migrate/20210512200517_add_fields_to_users.rb
@@ -2,6 +2,6 @@ class AddFieldsToUsers < ActiveRecord::Migration[6.1]
   def change
     add_column :users, :name, :string
     add_column :users, :username, :string
-    add_index :users, :usernmae, unique: true
+    add_index :users, :username, unique: true
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -29,9 +29,9 @@ ActiveRecord::Schema.define(version: 2021_05_12_210255) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
     t.string "username"
-    t.index "\"usernmae\"", name: "index_users_on_usernmae", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
 end
